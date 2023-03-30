f7002affeb506736e2cde13debebed921160d5eb
fabien382 <fabien.nvn@gmail.com>
2023-03-30 10:10:18 +0200

    Fix close dialog

--- src/components/organisms/messaging/CreateRoomDialog.vue
+++ src/components/organisms/messaging/CreateRoomDialog.vue
@@ -1,60 +1,64 @@
 <template>
-  <v-row justify="center">
-    <v-dialog
-      v-model="open"
-      class="o-createRoomDialog"
-      fullscreen
-      :scrim="false"
-      transition="dialog-bottom-transition"
-    >
-      <v-form>
-        <!-- XXX: padding is to fix v-card-actions scrollbar with dialog -->
-        <v-card class="pa-1" color="background">
-          <v-card-title class="flex-column align-stretch">
-            <v-row justify="center">
-              <v-col class="pb-0">
-                <div class="d-flex justify-end">
-                  <v-btn icon @click="open = false"> X </v-btn>
-                </div>
-              </v-col>
+  <v-dialog
+    v-model="open"
+    class="o-createRoomDialog"
+    fullscreen
+    :scrim="false"
+    transition="dialog-bottom-transition"
+  >
+    <v-form>
+      <!-- XXX: padding is to fix v-card-actions scrollbar with dialog -->
+      <v-card class="o-createRoomDialog__card pa-1" style="height: 100vh" color="background">
+        <v-card-title class="flex-column align-stretch">
+          <v-row justify="center">
+            <v-col class="pb-0">
+              <div class="d-flex justify-end">
+                <v-btn icon @click="open = false"> X </v-btn>
+              </div>
+            </v-col>
 
-              <v-col cols="12" class="pt-0 text-center text-h2"> Créer un groupe de message </v-col>
-            </v-row>
-          </v-card-title>
+            <v-col cols="12" class="pt-0 text-center text-h2"> Créer un groupe de message </v-col>
+          </v-row>
+        </v-card-title>
 
-          <v-card-text class="o-createRoomDialog__body">
-            <v-row>
-              <v-col cols="12">
-                <v-text-field v-model="fields.name" label="Nom du groupe" variant="solo" />
-              </v-col>
-              <v-col cols="12">
-                <v-select
-                  v-model="fields.members"
-                  clearable
-                  chips
-                  label="Select"
-                  :items="listMembers"
-                  item-title="lastname"
-                  item-value="_id"
-                  multiple
-                ></v-select>
-              </v-col>
-            </v-row>
-          </v-card-text>
+        <v-card-text class="o-createRoomDialog__body">
+          <v-row>
+            <v-col cols="12">
+              <v-text-field v-model="fields.name" label="Nom du groupe" variant="solo" />
+            </v-col>
+            <v-col cols="12">
+              <v-select
+                v-model="fields.members"
+                clearable
+                chips
+                label="Select"
+                :items="listMembers"
+                item-title="lastname"
+                item-value="_id"
+                multiple
+              ></v-select>
+            </v-col>
+          </v-row>
+        </v-card-text>
 
-          <v-card-actions>
-            <v-row justify="center">
-              <v-col cols="10" md="8">
-                <v-btn block color="teampulse" :loading="isLoading" x-large @click="createChatRoom">
-                  Créer
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-card-actions>
-        </v-card>
-      </v-form>
-    </v-dialog>
-  </v-row>
+        <v-card-actions>
+          <v-row justify="center">
+            <v-col cols="10" md="8">
+              <v-btn
+                block
+                style="background-color: rgb(35, 178, 164); color: white"
+                :loading="isLoading"
+                x-large
+                @click="createChatRoom"
+              >
+                Créer
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </v-dialog>
 </template>
 
 <script>
@@ -158,9 +162,11 @@ export default {
 
 <style lang="postcss" scoped>
 .o-createRoomDialog {
-  height: 100vh;
   &__body {
     overflow-y: auto;
+  }
+  &__card {
+    height: 100vh;
   }
 }
 </style>
