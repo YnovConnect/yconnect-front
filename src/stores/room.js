71fc942a5f300c0c34a22131b3f66c1ac0db6b0d
PikkMoune71 <antoinepicard385@gmail.com>
2023-03-31 16:12:00 +0200

    comments code

--- src/stores/room.js
+++ src/stores/room.js
@@ -1,39 +1,41 @@
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
 import api from '../utils/api'
 
 export const useRoomStore = defineStore({
   id: 'room',
   state: () => ({
-    currentRoomId: null,
+    currentRoomId: null
   }),
   actions: {
-
+    /**
+     * Create a new room
+     */
     async createRoom(name, userCreate, idUsers) {
       try {
-        const response = await api.post('rooms',
-        {
+        const response = await api.post('rooms', {
           name: name,
           userCreate: userCreate,
           idUsers: idUsers,
-          privateRoom: false,
-        },
-        );
+          privateRoom: false
+        })
 
-        return response.data;
-
+        return response.data
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
     },
 
+    /**
+     * Get all rooms
+     */
     async fetchRooms() {
       try {
-        const response = await api.get('rooms');
-        return response.data;
+        const response = await api.get('rooms')
+        return response.data
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
     }
   },
-  mutations: {  },
-});
+  mutations: {}
+})
